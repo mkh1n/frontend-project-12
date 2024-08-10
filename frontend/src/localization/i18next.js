9ca410f6cc1a35bc1c6a15d0f39ab64649f96617
Roman <romamuhino1@gmai.com>
2024-08-11 01:34:47 +0300

    fix i18n

--- frontend/src/localization/i18next.js
+++ frontend/src/localization/i18next.js
@@ -1,16 +1,17 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+//import LanguageDetector from 'i18next-browser-languagedetector'; 
 import resources from './resources';
 console.log(resources)
 i18next 
-  .use(initReactI18next) // передаем экземпляр i18n в react-i18next, который сделает его доступным для всех компонентов через context API.
-  .use(LanguageDetector) // с помощью плагина определяем язык пользователя в браузере
+  .use(initReactI18next) 
+  //.use(LanguageDetector)
   .init({
-    resources, // передаем переводы текстов интерфейса в формате JSON
-    fallbackLng: 'ru', // если переводы на языке пользователя недоступны, то будет использоваться язык, указанный в этом поле
+    resources,
+    lng: 'ru', 
+    fallbackLng: 'ru',
     interpolation: {
-      escapeValue: false, // экранирование уже есть в React, поэтому отключаем
+      escapeValue: false,
     },
     debug: true,
     supportedLngs: ['ru', 'en',],
