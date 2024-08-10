0623effcf6284c097913b4a4a3077d7d204d3838
Roman <romamuhino1@gmai.com>
2024-08-10 23:27:05 +0300

    add rollbar service

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -4,17 +4,6 @@ import SignupPage from './SignupPage';
 import ErrorPage from './ErrorPage';
 import MainPage from './mainPage/MainPage';
 import './App.css';
-import { Provider, ErrorBoundary } from '@rollbar/react' // Provider imports 'rollbar'
-
-const rollbarConfig = {
-  accessToken: '80fe158640cd4d8e9ea38371f1682cfd',
-  environment: 'testenv',
-}
-
-function TestError() {
-  const a = null
-  return a.hello()
-}
 
 function App() {
   return (
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { store } from './store';
 import i18n from './localization/i18next';
 import { I18nextProvider } from 'react-i18next';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider, ErrorBoundary } from '@rollbar/react' // Provider imports 'rollbar'
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'
 
 const rollbarConfig = {
   accessToken: '80fe158640cd4d8e9ea38371f1682cfd',
@@ -19,11 +19,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <I18nextProvider i18n={i18n}>
-        <Provider config={rollbarConfig}>
+        <RollbarProvider config={rollbarConfig}>
           <ErrorBoundary>
             <App />
           </ErrorBoundary>
-        </Provider>
+        </RollbarProvider>
       </I18nextProvider>
     </Provider>
   </React.StrictMode>
